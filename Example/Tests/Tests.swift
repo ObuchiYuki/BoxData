1cccc1d2cf615b3ab43c5121ae88447eda109f80
yuki <yukibochi1@gmail.com>
2019-09-10 12:25:14 +0900

    resion

--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -4,27 +4,24 @@ import Quick
 import Nimble
 @testable import BoxData
 
-class TableOfContentsSpec: QuickSpec {
-    // MARK: - Codable Data
-    struct Region: Codable {
-        let sections:[[[Section]]]
-        
-        struct Section: Codable, Equatable {
-            let anchor: UInt16
-            let fill: UInt16
-            let fillAnchor: UInt16
-            let data: UInt8
-        }
-    }
+// MARK: - Codable Data
+struct Region: Codable {
+    let sections:[[[Section]]]
     
-    private func createData() -> Region {
-        let air = Region.Section(anchor: 0, fill: 0, fillAnchor: 0, data: 0)
-        
-        return Region(sections: Array(repeating: Array(repeating: Array(repeating: air, count: 100), count: 5), count: 100) )
+    struct Section: Codable, Equatable {
+        let anchor: UInt16
+        let fill: UInt16
+        let fillAnchor: UInt16
+        let data: UInt8
     }
+}
+
+class TableOfContentsSpec: QuickSpec {
     
     func testNormal() {
-        let testData = createData()
+        let air = Region.Section(anchor: 0, fill: 0, fillAnchor: 0, data: 0)
+        
+        let testData = Region(sections: Array(repeating: Array(repeating: Array(repeating: air, count: 100), count: 5), count: 100) )
         
         let encoder = BoxEncoder()
         let decoder = BoxDecoder()
@@ -34,7 +31,7 @@ class TableOfContentsSpec: QuickSpec {
             
             let decoded = try decoder.decode(Region.self, from: data)
             
-            XCTAssertEqual(decoded.sections[0][0][0].anchor, 0)
+            XCTAssertEqual(decoded.sections[0][0][0], air)
             
         } catch {
             XCTFail("\(error)")
