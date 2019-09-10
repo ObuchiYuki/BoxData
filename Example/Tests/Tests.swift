858cefccc388ba9a226e6964e4c0229e0604e2a1
yuki <yukibochi1@gmail.com>
2019-09-10 12:27:29 +0900

    testNoCompressionNoStructure

--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -15,6 +15,51 @@ struct Region: Codable {
 
 class TableOfContentsSpec: XCTestCase {
     
+    func testNoCompressionNoStructure() {
+        let air = Region.Section(anchor: 0, fill: 0, fillAnchor: 0, data: 0)
+        
+        let testData = Region(sections: Array(repeating: Array(repeating: Array(repeating: air, count: 100), count: 5), count: 100) )
+        
+        let encoder = BoxEncoder()
+        encoder.useCompression = false
+        encoder.useStructureCache = false
+        let decoder = BoxDecoder()
+        
+        do {
+            let data = try encoder.encode(testData)
+            
+            let decoded = try decoder.decode(Region.self, from: data)
+            
+            XCTAssertEqual(decoded.sections[0][0][0], air)
+            
+        } catch {
+            XCTFail("\(error)")
+        }
+        
+    }
+    
+    func testNoCompression() {
+        let air = Region.Section(anchor: 0, fill: 0, fillAnchor: 0, data: 0)
+        
+        let testData = Region(sections: Array(repeating: Array(repeating: Array(repeating: air, count: 100), count: 5), count: 100) )
+        
+        let encoder = BoxEncoder()
+        encoder.useCompression = false
+        let decoder = BoxDecoder()
+        
+        do {
+            let data = try encoder.encode(testData)
+            
+            let decoded = try decoder.decode(Region.self, from: data)
+            
+            XCTAssertEqual(decoded.sections[0][0][0], air)
+            
+        } catch {
+            XCTFail("\(error)")
+        }
+        
+    }
+    
     func testNoStructure() {
         let air = Region.Section(anchor: 0, fill: 0, fillAnchor: 0, data: 0)
         
