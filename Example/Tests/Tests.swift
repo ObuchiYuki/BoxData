486592a8cde310ab9237d100e0ca78dac12ce6bf
yuki <yukibochi1@gmail.com>
2019-09-10 12:23:56 +0900

    Equatable

--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -9,7 +9,7 @@ class TableOfContentsSpec: QuickSpec {
     struct Region: Codable {
         let sections:[[[Section]]]
         
-        struct Section: Codable {
+        struct Section: Codable, Equatable {
             let anchor: UInt16
             let fill: UInt16
             let fillAnchor: UInt16
@@ -20,14 +20,25 @@ class TableOfContentsSpec: QuickSpec {
     private func createData() -> Region {
         let air = Region.Section(anchor: 0, fill: 0, fillAnchor: 0, data: 0)
         
-        Region(sections:
-        )
+        return Region(sections: Array(repeating: Array(repeating: Array(repeating: air, count: 100), count: 5), count: 100) )
     }
     
     func testNormal() {
+        let testData = createData()
+        
         let encoder = BoxEncoder()
         let decoder = BoxDecoder()
         
+        do {
+            let data = try encoder.encode(testData)
+            
+            let decoded = try decoder.decode(Region.self, from: data)
+            
+            XCTAssertEqual(decoded.sections[0][0][0].anchor, 0)
+            
+        } catch {
+            XCTFail("\(error)")
+        }
         
     }
 }
