78dd59fa21d1cf5bf32270d5a06f06e9ffe34e7c
yuki <yukibochi1@gmail.com>
2019-09-10 10:35:53 +0900

    macOS

---
+++ Example/Pods/BoxData macOS/BoxData_macOS.h
@@ -0,0 +1,18 @@
+//
+//  BoxData_macOS.h
+//  BoxData macOS
+//
+//  Created by yuki on 2019/09/10.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BoxData_macOS.
+FOUNDATION_EXPORT double BoxData_macOSVersionNumber;
+
+//! Project version string for BoxData_macOS.
+FOUNDATION_EXPORT const unsigned char BoxData_macOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BoxData_macOS/PublicHeader.h>
+
+
